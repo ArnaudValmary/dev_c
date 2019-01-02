afea35858651ca393288258b79ca4142b8cb9bdb
Arnaud Valmary <arnaud.valmary@free.fr>
2019-01-02 10:15:29 +0100

    Define _GNU_SOURCE and use __GNUC__ macros

--- libmythread/src/mythreadIsMainThread.c
+++ libmythread/src/mythreadIsMainThread.c
@@ -1,13 +1,24 @@
 
 #include <libmythread/libmythread.h>
 
+#ifdef __GNUC__
+
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 
+#endif
+
 boolean_t mythreadIsMainThread(void) {
+#ifdef __GNUC__
     if (syscall(SYS_gettid) == getpid()) {
         return TRUE;
     }
     return FALSE;
+#else
+    if (thr_self() == 1) {
+        return TRUE;
+    }
+    return FALSE;
+#endif
 }
