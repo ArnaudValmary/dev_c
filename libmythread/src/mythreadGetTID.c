555c69fdf3245d6337c7eac749530ceb36019e10
Arnaud Valmary <arnaud.valmary@free.fr>
2019-01-02 10:13:27 +0100

    Define _GNU_SOURCE and use __GNUC__ macros

--- libmythread/src/mythreadGetTID.c
+++ libmythread/src/mythreadGetTID.c
@@ -1,9 +1,18 @@
 
 #include <libmythread/libmythread.h>
 
+#ifdef __GNUC__
+
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <sys/syscall.h>
 
+#endif
+
 mythread_tid_t mythreadGetTID(void) {
+#ifdef __GNUC__
     return syscall(SYS_gettid);
+#else
+    return thr_self();
+#endif
 }
